7bc58230058f230b1f78c8ba9bc112d306588258
harshaarajarajan <hbr44349#uga.edu>
2026-08-27 14:49:32 -0400

    updated com port on dash

--- H.A.R.S.H V1/dashboard.py
+++ H.A.R.S.H V1/dashboard.py
@@ -5,7 +5,7 @@ from PySide6.QtWidgets import (QApplication, QMainWindow, QLabel, QVBoxLayout,
 from PySide6.QtCore import QTimer, Qt
 from PySide6.QtGui import QFont
 
-COM_PORT = 'COM8'
+COM_PORT = 'COM10'
 BAUD_RATE = 115200
 
 class OBDDashboard(QMainWindow):
